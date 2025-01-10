75a2ba5d8e06384e93cdf67a2da10e206dd06537
cuter97 <aguscastets@gmail.com>
2025-01-10 14:56:43 -0300

    change h1, test commit

--- src/components/LoadingQuestions.tsx
+++ src/components/LoadingQuestions.tsx
@@ -40,7 +40,7 @@ export const LoadingQuestions = ({ finished }: Props) => {
         <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[70vw] md:w-[60vw] flex flex-col items-center">
             <Image src={"/loading.gif"} width={400} height={400} alt="loading" unoptimized/>
             <Progress value={progress} className="w-full mt-4" />
-            <h1 className="mt-2 text-xl">{loadingText}</h1>
+            <h1 className="mt-2 text-xl">{loadingText} esto es un test</h1>
         </div>
     );
 };
